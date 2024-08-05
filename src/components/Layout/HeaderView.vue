1a467bad963d873c061bec4002b2cc7bb796acba
DESKTOP-5353HBD\plae5 <play56782@gmail.com>
2024-08-05 21:21:59 +0800

    1.導覽列 scroll-event finish 2.新增pinia(scroll-event) 3.修正header及footer部分scss

--- src/components/Layout/HeaderView.vue
+++ src/components/Layout/HeaderView.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
+    <header :class="{ 'transparent': scrollStore.isTransparent }" ref="navScroll">
         <div class="container">
             <nav class="navigation">
                 <RouterLink to="/" class="logoIndex">
@@ -33,6 +33,27 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import { RouterLink } from 'vue-router'
+import { useScrollStore } from '../../stores/homeScroll'
+
+const navScroll = ref(null);
+const scrollStore = useScrollStore();
+
+onMounted(() => {
+    scrollStore.setNavRef(navScroll.value);
+});
 
 </script>
+
+<style lang="scss" scoped>
+.transparent {
+    transition: background-color 0.3s, color 0.3s;
+    background-color: rgba(117, 149, 172, 0.7);
+
+    .navbar a,
+    .loginbar a {
+        color: #fff;
+    }
+}
+</style>
---
+++ src/stores/homeScroll.js
@@ -0,0 +1,18 @@
+//scroll-event
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useScrollStore = defineStore('scroll', () => {
+  const isTransparent = ref(false);
+  const navRef = ref(null);
+
+  const setTransparent = (value) => {
+    isTransparent.value = value;
+    
+  };
+  const setNavRef = (ref) => {
+    navRef.value = ref;
+  };
+
+  return { isTransparent, setTransparent,navRef,setNavRef };
+});
