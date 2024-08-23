954ffeccda00e189e7e13c8e25beb0d7625be90a
DESKTOP-5353HBD\plae5 <play56782@gmail.com>
2024-08-23 18:15:47 +0800

    1.DestinationTitle search history add remove Function 2.修正Destinationscss部分樣式 3.DestinationPopularity add Pagination Function && update DestinationPopularity .json && html add prevage and next

--- src/components/Destination/DestinationPopularity.vue
+++ src/components/Destination/DestinationPopularity.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <h2>人氣景點</h2>
             <div class="cardList">
-                <div class="card" v-for="(card, index) in cardData" :key="index">
+                <div class="card" v-for="(card, index) in limitCardData" :key="index">
                     <div class="image">
                         <img :src="card.picture">
                     </div>
@@ -23,6 +23,10 @@
                     <RouterLink>查看完整資訊</RouterLink>
                 </div>
             </div>
+            <div class="pageControl">
+                <i class="fa-solid fa-left-long" @click="prevPage" :class="{ disable: firstPage }"></i>
+                <i class="fa-solid fa-right-long" @click="nextPage" :class="{ disable: lastPage }"></i>
+            </div>
             <div class="more">
                 <RouterLink to="#"><i class="fa-solid fa-angles-right"></i>more</RouterLink>
 
@@ -32,10 +36,40 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { RouterLink } from 'vue-router';
 
 const cardData = ref([]);
+const limitShow = ref(4);   //限制顯示筆數
+const currentIndex = ref(1)   //當前頁
+
+const limitCardData = computed(() => {
+    const start = (currentIndex.value - 1) * limitShow.value;
+    const end = start + limitShow.value;
+    return cardData.value.slice(start, end)
+})
+//計算總頁數
+const totalPage = computed(() => {
+    return cardData.value.length / limitShow.value
+});
+//限制禁用功能
+const firstPage = computed(() => {
+    return currentIndex.value === 1;
+});
+const lastPage = computed(() => {
+    return currentIndex.value === totalPage.value;
+});
+const prevPage = () => {
+    if (currentIndex.value > 1) {
+        currentIndex.value--;
+    }
+};
+const nextPage = () => {
+    if (currentIndex.value < totalPage.value) {
+        currentIndex.value++;
+    }
+};
+
 
 onMounted(async () => {
     try {
@@ -44,8 +78,11 @@ onMounted(async () => {
             throw new Error('未取得資訊，請重新嘗試');
         }
         cardData.value = await response.json();
+
     } catch (error) {
         console.log('錯誤', error);
     }
 });
+
+
 </script>
--- src/components/Destination/DestinationTitle.vue
+++ src/components/Destination/DestinationTitle.vue
@@ -6,12 +6,15 @@
                     <h1>腳步起舞：即刻啟程，精心規劃</h1>
                     <span>發現無盡的驚喜，揭開神秘的面紗</span>
                     <div class="searchBar">
-                        <input type="search">
-                        <RouterLink>搜尋</RouterLink>
+                        <input type="search" v-model="searchHistory" @keydown.enter="addSearchHistory">
+                        <button @click="addSearchHistory">搜尋</button>
                     </div>
                 </div>
                 <div class="history">
-                    <button v-for="(item, index) in btns" :key="index">{{ item }}</button>
+                    <button v-for="(item, index) in btns" :key="index">
+                        {{ item }}
+                        <i class="fa-solid fa-rectangle-xmark" @click="delHistory(item)"></i>
+                    </button>
                 </div>
             </div>
         </div>
@@ -20,7 +23,29 @@
 
 <script setup>
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
 
 const btns = ref(['太魯閣國家公園', '九份老街', '日月潭', '清境農場', '阿里山', '墾丁', '台中逢甲夜市'])
+const searchHistory = ref('')
+
+const addSearchHistory = () => {
+    //移除空格檢查
+    if (searchHistory.value.trim() !== '') {
+        //排除重複搜尋結果，於陣列中找尋
+        if (!btns.value.includes(searchHistory.value)) {
+            //限制頁面顯示最多6筆紀錄
+            if (btns.value.length >= 6) {
+                btns.value.pop();
+            }
+            btns.value.unshift(searchHistory.value)
+        }
+        searchHistory.value = ''
+    }
+}
+const delHistory = (removeValue) => {
+    const indexValue = btns.value.indexOf(removeValue)
+    if (indexValue > -1) {
+        btns.value.splice(indexValue, 1)
+    }
+}
+
 </script>
